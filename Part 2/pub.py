a3f711fa4df587e42b9882d03fd2c7dfb4e34988
JD-007 <jedidiah.oluwafemi@usask.ca>
2025-01-26 23:46:46 -0600

    Init

---
+++ Part 2/pub.py
@@ -0,0 +1,57 @@
+import time
+from datetime import datetime
+
+import paho.mqtt.client as mqtt
+from cryptography.fernet import Fernet
+from paho.mqtt import client as mqtt_client
+
+# broker = "broker.hivemq.com"
+# broker = "test.mosquitto.org"
+broker = "broker.emqx.io"
+# broker = "iot.coreflux.cloud"
+
+port = 1883
+topic = "python/mqtt"
+
+cipher_key = 'aeFXbGYE7ng_wvnaq9IOOqBM6S6Q45_Jo0bHmSHWYYs='
+cipher = Fernet(cipher_key)
+
+
+def connect_mqtt():
+    def on_connect(a, b, c, d, e):
+        print("Connected to MQTT Broker!")
+
+    client = mqtt_client.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client):
+    msg_count = 1
+    while True:
+        time.sleep(0.2)
+        actual_message = str(datetime.timestamp(datetime.now()))
+        out_message = actual_message.encode()
+        encrypted_message = cipher.encrypt(out_message)
+        result = client.publish(topic=topic, payload=encrypted_message)
+        status = result[0]
+        if status != 0:
+            print(f"Failed to send message to topic {topic}")
+        else:
+            msg_count += 1
+        if msg_count > 40:
+            break
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+    client.loop_stop()
+    client.disconnect()
+
+
+if __name__ == '__main__':
+    run()
---
+++ Part 2/sub.py
@@ -0,0 +1,57 @@
+import sys
+from datetime import datetime
+
+import paho.mqtt.client as mqtt
+from cryptography.fernet import Fernet
+from paho.mqtt import client as mqtt_client
+
+#broker = "broker.hivemq.com"
+#broker = "test.mosquitto.org"
+broker = "broker.emqx.io"
+#broker = "iot.coreflux.cloud"
+
+port = 1883
+topic = "python/mqtt"
+
+cipher_key = 'aeFXbGYE7ng_wvnaq9IOOqBM6S6Q45_Jo0bHmSHWYYs='
+cipher = Fernet(cipher_key)
+
+l = []
+
+def connect_mqtt():
+    def on_connect(a, b, c, d, e):
+        print("Connected to MQTT Broker!")
+
+    client = mqtt_client.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def subscribe(client: mqtt_client):
+    def on_message(client, user_data,message):
+        b = message.payload.decode('utf-8')
+        decrypted_msg = cipher.decrypt(b)
+        curr_time = datetime.timestamp(datetime.now())
+
+        l.append(curr_time - float(decrypted_msg))
+
+        if len(l) == 40:
+            print("Latency is", sum(l) / len(l))
+            client.disconnect()
+            sys.exit()
+
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
